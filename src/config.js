20974563a22ce1df96e898ac682776e6c5b4c385
AndreaZero.eth <100024338+AndreaZero@users.noreply.github.com>
2023-08-18 23:01:17 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 //   BASE_URL = "http://localhost:4000/";
 // }
-let BASE_URL = "https://mysterious-retreat-58916-cd3de62c97ef.herokuapp.com";
+let BASE_URL = "https://mysterious-retreat-58916-cd3de62c97ef.herokuapp.com/";
 
 export { BASE_URL };
