eff0b94dda2d56da158023d8f2430c363f1291ab
AndreaZero.eth <100024338+AndreaZero@users.noreply.github.com>
2023-08-18 21:50:40 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 //   BASE_URL = "http://localhost:4000/";
 // }
-let BASE_URL = "http://localhost:3000/";
+let BASE_URL = "https://sleepy-wave-51018-dad62363f720.herokuapp.com/";
 
 export { BASE_URL };
