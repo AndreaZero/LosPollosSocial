f71b42f225428d1b591e942dea2544fc20ce03b9
AndreaZero.eth <100024338+AndreaZero@users.noreply.github.com>
2023-08-18 22:39:19 +0200

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// let BASE_URL = "https://post-it-heroku.herokuapp.com/";
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+//   BASE_URL = "http://localhost:4000/";
+// }
+let BASE_URL = "https://mysterious-retreat-58916-cd3de62c97ef.herokuapp.com/";
+
+export { BASE_URL };
