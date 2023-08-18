3aaf5ffd853c92c3e6628d3338a2aa6689831241
AndreaZero.eth <100024338+AndreaZero@users.noreply.github.com>
2023-08-18 22:11:13 +0200

    Delete src directory

--- src/config.js
+++
@@ -1,7 +0,0 @@
-// let BASE_URL = "https://post-it-heroku.herokuapp.com/";
-// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-//   BASE_URL = "http://localhost:4000/";
-// }
-let BASE_URL = "https://sleepy-wave-51018-dad62363f720.herokuapp.com/";
-
-export { BASE_URL };
